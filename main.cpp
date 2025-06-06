0dbc36fe238478c6bc0bb075e08dc6cd41b5e9d5
Philip Hoderny <pokemonphilip8647@gmail.com>
2025-06-06 01:25:02 -0400

    Changed the gui library to be rlImGui which required DearImGui as a dependancy so i added both as well as trying to draw a grid of blocks on the screen but that's unsuccessful so far

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "raylib.h"
-#include "raygui.h"
-#include "style_dark.h"
+#include "rlImGui.h"
 #include "vector"
+#include <iostream>
+
 
 int main() {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 450, "raylib [core] example - basic window");
-    GuiLoadStyleDark(); // TODO: Replace rayGui with rlImGui because apparently rayGui is only for c and not c++
+    rlImGuiSetup(true);
+
 
     struct Block {
         Rectangle rectangle;
@@ -21,27 +23,48 @@ int main() {
 
     blockGrid.reserve(blockGridSize);
 
-    for (int i=0; i<blockGridSize; i++) {
-        blockGrid.push_back(Block { // TODO: implement actual proper grid creation alignment using modulo and other functions
-            static_cast<float>(GetRandomValue(0, GetScreenWidth())),
-            static_cast<float>(GetRandomValue(GetScreenHeight() / 2, GetScreenHeight())),
-            static_cast<float>(GetScreenWidth()) / blockGridWidth,
-            static_cast<float>(GetScreenHeight()) / blockGridHeight,
-            RAYWHITE
-        }); //
-    }
+
 
     int windowWidth = GetScreenWidth();
     int windowHeight = GetScreenHeight();
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
+
+
         BeginDrawing();
         ClearBackground(DARKGRAY);
+
+        rlImGuiBegin();
+        rlImGuiEnd();
 
         if (windowWidth != GetScreenWidth() || windowHeight != GetScreenHeight()) {
             windowWidth = GetScreenWidth();
             windowHeight = GetScreenHeight();
+
+            blockGrid.clear();
+            /*
+            for (int i=0; i<blockGridSize; i++) {
+                blockGrid.push_back(Block { // TODO: implement actual proper grid creation alignment using modulo and other functions
+                    static_cast<float>(GetRandomValue(0, GetScreenWidth())),
+                    static_cast<float>(GetRandomValue(GetScreenHeight() / 2, GetScreenHeight())),
+                    static_cast<float>(GetScreenWidth()) / blockGridWidth,
+                    static_cast<float>(GetScreenHeight()) / blockGridHeight,
+                    RAYWHITE
+                }); //
+            }
+            */
+
+            float blockWidth = GetScreenWidth() / blockGridWidth;
+            float blockHeight = GetScreenHeight() / blockGridHeight;
+
+            for (int i=0; i<blockGridWidth; i++) {
+                blockGrid.push_back(Block {
+                    static_cast<float>((i % blockGridSize) * blockWidth),
+                    static_cast<float>((i / blockGridSize) * blockHeight),
+                    static_cast<float>(blockWidth),
+                    static_cast<float>(blockHeight),
+                });
+            }
         }
 
         for (int i=0; i<blockGridSize; i++) {
@@ -52,6 +75,7 @@ int main() {
     }
 
     CloseWindow();
+    rlImGuiShutdown();
 
     return 0;
 }
