67e804a7da7676ad3c6b77002ed79cddfe14c357
Philip Hoderny <pokemonphilip8647@gmail.com>
2025-06-07 00:56:44 -0400

    Fixed the issue with the block grid not filling the screen. Added some debug info to the GUI, namely the number of blocks and the aspect ratio of the current block grid.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "rlImGuiColors.h"
 #include "vector"
 #include "imgui.h"
+#include <cmath>
+#include <numeric>
 #include <iostream>
 
 
@@ -12,14 +14,14 @@ struct Block {
     unsigned int health;
 };
 
-void recalculateBlockGrid(int blockGridWidth, int blockGridHeight, int& blockGridSize, std::vector<Block>& blockGrid) { // TODO: Fix the bug that prevents the grid from occupying the whole screen in certain window sizes and grid sizes
+void recalculateBlockGrid(int blockGridWidth, int blockGridHeight, int& blockGridSize, std::vector<Block>& blockGrid) {
     blockGridSize = blockGridHeight * blockGridWidth;
 
     blockGrid.clear();
 
     // if you static_cast to float for these values it causes no rounding so the blocks end up having spaces around them
-    float blockWidth = GetScreenWidth() / blockGridWidth;
-    float blockHeight = GetScreenHeight() / blockGridHeight;
+    float blockWidth = ceil(static_cast<float>(GetScreenWidth()) / static_cast<float>(blockGridWidth));
+    float blockHeight = ceil(static_cast<float>(GetScreenHeight()) / static_cast<float>(blockGridHeight));
 
 
     for (int i=0; i<blockGridSize; i++) {
@@ -42,22 +44,56 @@ void recalculateBlockGrid(int blockGridWidth, int blockGridHeight, int& blockGri
     }
 }
 
+int getNumberOfDigits(int num) {
+    if (num != 0) {
+        return log10(num);
+    }
+    else {
+        return 1;
+    }
+}
+
+char* findAspectRatio(int width, int height) { // Since I'm working with char* I just decided to use a mix of c and c++ as I'm most comfortable with c when I'm dealing with them
+    int gcd = std::gcd(width, height);
+    // std::cout << "gcd: " << gcd << std::endl;
+
+    int numOfDigits;
+    int resultStringLength;
+
+    numOfDigits = getNumberOfDigits(width / gcd);
+
+    char* temp = (char*)malloc(numOfDigits * sizeof(char));
+
+    resultStringLength = getNumberOfDigits(numOfDigits + (height / gcd) + 1);
+    char* result = (char*)malloc(numOfDigits * sizeof(char)); // I don't free the result char* as it is what's returned for the function
+
+    sprintf(result, "%d", width / gcd);
+    sprintf(temp, "%d", height / gcd);
+
+    result = strcat(result, ":");
+    result = strcat(result, temp);
+
+    free(temp);
+    return result;
+}
+
 int main() {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 450, "raylibTesting");
     rlImGuiSetup(true);
+    SetExitKey(0);
 
     std::vector<Block> blockGrid;
 
-    int blockGridMinWidth = 5;
-    int blockGridMinHeight = 4;
-    int blockGridMaxWidth = 432;
-    int blockGridMaxHeight = 240;
+    int blockGridMinWidth = 16;
+    int blockGridMinHeight = 9;
+    int blockGridMaxWidth = 320;
+    int blockGridMaxHeight = 180;
     int blockGridWidth = 30;
     int blockGridHeight = 20;
     int blockGridSize = blockGridMaxWidth * blockGridMaxHeight;
 
-    float backgroundColor[3] = {80, 80, 80};
+    float backgroundColor[3] = {80.0f / 255.0f, 80.0f / 255.0f, 80.0f / 255.0f};
 
     blockGrid.reserve(blockGridSize);
 
@@ -104,6 +140,9 @@ int main() {
             if (ImGui::SliderInt("Block grid height slider", &blockGridHeight, blockGridMinHeight, blockGridMaxHeight)) {
                 recalculateBlockGrid(blockGridWidth, blockGridHeight, blockGridSize, blockGrid);
             }
+
+            ImGui::Text("Total Blocks: %d", blockGridSize);
+            ImGui::Text("Grid aspect ratio: %s", findAspectRatio(blockGridWidth, blockGridHeight));
 
             ImGui::Text("Background color");
             ImGui::ColorEdit3("Background color", backgroundColor);
